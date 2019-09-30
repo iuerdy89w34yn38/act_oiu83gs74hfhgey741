fb70f54aedc47b9fea35667954e7d02c5e39fd48
projrx <projrx@hamzapervaiz.com>
2019-09-30 13:48:04 +0500

    work

---
+++ temp/test/test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+
+    $("input[type$='number']").attr("step", "0.01");
+
+});
+</script>
+</head>
+<body>
+
+<br>
+<input type="number" >
+<br><br>
+<br>
+<br>
+<br>
+
+<input type="text" value="man" >
+
+<br>
+<br>
+
+<button>Set the width attribute of the image</button>
+
+</body>
+</html>
